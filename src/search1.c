ee80d6b2c5725de141e0285f20ec120ec5a1569a
talhagencc <genc.talha@ibu.edu.mk>
2026-03-13 23:48:10 +0000

    i finished my homework

--- src/search1.c
+++ src/search1.c
@@ -18,3 +18,28 @@ Rules:
 - You must use strcmp().
 - Do NOT use == for string comparison.
 */
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    // An array of strings
+    char *strings[] = {"battleship", "boot", "cannon", "iron", "thimble", "top hat"};
+
+    // Search for string
+    char s[100];
+    printf("String: ");
+    scanf("%99s", s);
+
+    for (int i = 0; i < 6; i++)
+    {
+        if (strcmp(strings[i], s) == 0)
+        {
+            printf("Found\n");
+            return 0;
+        }
+    }
+
+    printf("Not found\n");
+    return 1;
+}
