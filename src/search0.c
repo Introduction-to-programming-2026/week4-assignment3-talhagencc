e2c710a39ac3e77dbc46177590c68a38a9a51ebe
talhagencc <genc.talha@ibu.edu.mk>
2026-03-13 23:43:50 +0000

    i finished homework

--- src/search0.c
+++ src/search0.c
@@ -17,3 +17,27 @@ Rules:
 - You must use a loop.
 - Do NOT use any built-in search function.
 */
+#include <stdio.h>
+
+int main(void)
+{
+    // An array of integers
+    int numbers[] = {20, 500, 10, 5, 100, 1, 50};
+
+    // Search for number
+    int n;
+    printf("Number: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < 7; i++)
+    {
+        if (numbers[i] == n)
+        {
+            printf("Found\n");
+            return 0;
+        }
+    }
+
+    printf("Not found\n");
+    return 1;
+}
