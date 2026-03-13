1a2bd41cd12a20717a80a2881f4effddcdb5bdb2
talhagencc <genc.talha@ibu.edu.mk>
2026-03-13 23:29:10 +0000

    i finished my homework

--- src/phonebook.c
+++ src/phonebook.c
@@ -20,3 +20,29 @@ Rules:
 - You must use struct.
 - Use strcmp() to compare names.
 */
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    // Arrays of strings
+    char names[3][20] = {"Kelly", "David", "John"};
+    char numbers[3][20] = {"+1-617-495-1000", "+1-617-495-1000", "+1-949-468-2750"};
+
+    // Search for name
+    char name[20];
+    printf("Name: ");
+    scanf("%19s", name); // CS50 get_string yerine
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (strcmp(names[i], name) == 0)
+        {
+            printf("Found %s\n", numbers[i]);
+            return 0;
+        }
+    }
+
+    printf("Not found\n");
+    return 1;
+}
