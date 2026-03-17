3c10c577f426c00d7a16dbbe5c5bf676f4fa479f
talhagencc <genc.talha@ibu.edu.mk>
2026-03-17 09:58:38 +0000

    finish

--- src/recursion.c
+++ src/recursion.c
@@ -1,23 +1,5 @@
-/*
-WEEK 1 — TASK 2 (Recursion)
+// Draws a pyramid using recursion (without CS50)
 
-Goal:
-Practice recursion.
-
-Task:
-1. Ask the user for a positive integer n.
-2. Use a recursive function to print:
-
-#
-##
-###
-####
-
-Rules:
-- You must create a recursive function.
-- You cannot use a loop to print rows.
-- Base case must be correct.
-*/
 #include <stdio.h>
 
 void draw(int n);
@@ -32,6 +14,8 @@ int main(void)
 
     // Draw pyramid
     draw(height);
+
+    return 0;
 }
 
 void draw(int n)
